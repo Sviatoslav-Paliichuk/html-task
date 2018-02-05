410dfa629b3cbf2adfcfcac3d6eedf4e91c9826d
Sviatoslav Paliichuk <spaliichuk@spaliichuk2-wf.hw.griddynamics.net>
2018-02-05 18:14:08 +0200

    added image slider

---
+++ resources/js/image-slider/image-slider.js
@@ -0,0 +1,61 @@
+var currentImageIndex = 0;
+var collections = [
+    {
+        id: 1,
+        path: "../resources/images/product-items/1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },
+    {
+        id: 2,
+        path: "../resources/images/product-items/2.jpg",
+        description: "Alias aperiam culpa error facilis maiores nisi. Lorem ipsum dolor sit amet."
+    },
+    {
+        id: 3,
+        path: "../resources/images/product-items/3.jpg",
+        description: "Optio reiciendis repellend us soluta voluptate. Alias aperiam culpa error."
+    }
+];
+
+function nextImage() {
+    currentImageIndex++;
+
+    var currentImage = document.getElementById("product-card__image-slider");
+    var currentImageLabel = document.getElementById("product-card__image-label");
+
+    if (currentImageIndex === collections.length) {
+        currentImageIndex = 0;
+    }
+
+    currentImage.setAttribute("src", collections[currentImageIndex]["path"]);
+    currentImageLabel.innerText = collections[currentImageIndex]["description"];
+    reset_animation();
+}
+
+function prevImage() {
+    var currentImage = document.getElementById("product-card__image-slider");
+    var currentImageLabel = document.getElementById("product-card__image-label");
+
+    if (currentImageIndex <= 0) {
+        currentImageIndex = collections.length;
+    }
+    currentImageIndex--;
+    currentImage.setAttribute("src", collections[currentImageIndex]["path"]);
+    currentImageLabel.innerText = collections[currentImageIndex]["description"];
+    reset_animation();
+}
+
+function reset_animation() {
+    var el = document.getElementById('product-card__image-label');
+    var el2 = document.getElementById('product-card__image-slider');
+
+    el.style.animation = 'none';
+    el.offsetHeight;
+    /* trigger reflow */
+    el.style.animation = null;
+
+    el2.style.animation = 'none';
+    el2.offsetHeight;
+    /* trigger reflow */
+    el2.style.animation = null;
+}
