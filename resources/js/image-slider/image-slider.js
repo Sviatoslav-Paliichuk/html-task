03cfda59463784e9a647a6454a8b2140839278d4
Sviatoslav Paliichuk <spaliichuk@spaliichuk2-wf.hw.griddynamics.net>
2018-02-27 17:21:43 +0200

    refactoring

---
+++ resources/js/image-slider/image-slider.js
@@ -0,0 +1,110 @@
+class CustomSlider {
+    constructor(sliderConfig) {
+        this.imageCollection = null;
+        this.currentImageIndex = 0;
+        this.imageCollection = sliderConfig.images;
+        this.watermarkConfig = sliderConfig.watermarkConfig;
+        this.width = sliderConfig.width;
+        this.height = sliderConfig.height;
+        this.isWatermarkVisible = sliderConfig.isWatermarkVisible;
+        this.imageObj = new Image();
+        this.renderTemplate(sliderConfig.containerSelector);
+    }
+
+    renderTemplate(containerId) {
+        this.loadTemplate(function (response) {
+            this.renderMarkup(containerId, response);
+            this.renderImage();
+            this.initializeConstantNodes();
+            this.createHandlers();
+        }.bind(this));
+    }
+
+    renderMarkup(containerId, response) {
+        const sliderContainer = document.getElementById(containerId);
+        sliderContainer.innerHTML = response;
+    }
+
+    initializeConstantNodes() {
+        this.nextImageButton = document.querySelector(NEXT_IMAGE_BUTTON);
+        this.previousImageButton = document.querySelector(PREVIOUS_IMAGE_BUTTON);
+        this.currentImage = document.querySelector(CURRENT_IMAGE);
+        this.imageLabel = document.querySelector(CURRENT_IMAGE_LABEL);
+        this.currentLabel = document.querySelector(CURRENT_IMAGE_LABEL);
+    }
+
+    createHandlers() {
+        this.nextImageButton.addEventListener('click', function () {
+            this.nextImage();
+            this.resetAnimation();
+            this.renderCanvasLabel();
+        }.bind(this), false);
+
+        this.previousImageButton.addEventListener('click', function () {
+            this.previousImage();
+            this.resetAnimation();
+            this.renderCanvasLabel();
+        }.bind(this), false);
+    }
+
+    loadTemplate(callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "../resources/js/image-slider/image-slider-template.html");
+        xhr.onload = function () {
+            if (xhr.readyState === 4 && xhr.status === 200)
+                callback(xhr.responseText);
+        }
+        xhr.send(null);
+    }
+
+    nextImage() {
+        this.currentImageIndex++;
+        this.currentImageIndex = this.currentImageIndex !== this.imageCollection.length
+            ? this.currentImageIndex
+            : this.currentImageIndex = 0;
+        this.renderImage();
+    }
+
+    previousImage() {
+        this.currentImageIndex = this.currentImageIndex > 0
+            ? this.currentImageIndex
+            : this.currentImageIndex = this.imageCollection.length;
+        this.currentImageIndex--;
+        this.renderImage();
+    }
+
+    renderImage() {
+        let currentImageObj = this.imageCollection[this.currentImageIndex];
+        this.imageObj.onload = function () {
+            this.renderCanvas();
+        }.bind(this);
+        this.imageObj.src = currentImageObj.path;
+    }
+
+    renderCanvas() {
+        let canvas = document.querySelector(SLIDER_SELECTOR);
+        canvas.context = canvas.getContext("2d");
+        canvas.width = this.width;
+        canvas.height = this.height;
+        canvas.context.drawImage(this.imageObj, 0, 0);
+        if (this.watermarkConfig && this.isWatermarkVisible) {
+            canvas.context.font = this.watermarkConfig.font;
+            canvas.context.fillStyle = this.watermarkConfig.fillStyle;
+            canvas.context.fillText(this.watermarkConfig.Text, this.watermarkConfig.labelPositionX, this.watermarkConfig.labelPositionY);
+        }
+    }
+
+    renderCanvasLabel() {
+        let currentImageObj = this.imageCollection[this.currentImageIndex];
+        this.currentLabel.innerText = currentImageObj.description;
+    }
+
+    resetAnimation() {
+        this.imageLabel.style.animation = 'none';
+        this.imageLabel.offsetHeight; // force repaint reflow for render animation
+        this.imageLabel.style.animation = null;
+        this.currentImage.style.animation = 'none';
+        this.currentImage.offsetHeight;
+        this.currentImage.style.animation = null;
+    }
+}
---
+++ resources/js/image-zoom/image-zoom.js
@@ -0,0 +1,129 @@
+class ImageZoom {
+    constructor(objectToZoomId, resultOfZoomId) {
+        this.objectToZoomID = objectToZoomId;
+        this.resultOfZoomID = resultOfZoomId;
+        this.dataURL = null;
+        this.lensWindow = null;
+        this.coordinateX = null;
+        this.coordinateY = null;
+        this.createdZoomLens = false;
+        this.zoomLensSelector = 'img-zoom-lens';
+        this.imageZoomContainer = '.img-zoom__container';
+    }
+
+    initializeZoomContainer() {
+        this.objectToZoom = document.querySelector(this.objectToZoomID);
+        this.resultOfZoom = document.getElementById(this.resultOfZoomID);
+    }
+
+    create() {
+        const _this = this;
+        if (!this.createdZoomLens) {
+            this.initializeZoomContainer();
+            this.createLensWindow();
+            this.createOutputWindow();
+            createLensHandlers();
+        }
+
+        _this.showZoomLens();
+        _this.showOutputWindow();
+        _this.createImageSliderHandlers();
+
+        function getCursorPos(e) {
+            let clientRect, x = 0, y = 0;
+            e = e || window.event;
+
+            clientRect = _this.objectToZoom.getBoundingClientRect();
+            x = e.pageX - clientRect.left;
+            y = e.pageY - clientRect.top;
+
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+
+            return {x: x, y: y};
+        }
+
+        function moveLens(e) {
+            let cursorPosition, x, y;
+            e.preventDefault();
+
+            cursorPosition = getCursorPos(e);
+            x = cursorPosition.x - (_this.lensWindow.offsetWidth / 2);
+            y = cursorPosition.y - (_this.lensWindow.offsetHeight / 2);
+
+            if (x > _this.objectToZoom.width - _this.lensWindow.offsetWidth) {
+                x = _this.objectToZoom.width - _this.lensWindow.offsetWidth;
+            }
+            if (x < 0) {
+                x = 0;
+            }
+            if (y > _this.objectToZoom.height - _this.lensWindow.offsetHeight) {
+                y = _this.objectToZoom.height - _this.lensWindow.offsetHeight;
+            }
+            if (y < 0) {
+                y = 0;
+            }
+
+            _this.lensWindow.style.left = x + "px";
+            _this.lensWindow.style.top = y + "px";
+            _this.resultOfZoom.style.backgroundPosition = "-" + (x * _this.coordinateX) + "px -" + (y * _this.coordinateY) + "px";
+        }
+
+        function createLensHandlers() {
+            _this.lensWindow.addEventListener("mousemove", moveLens);
+            _this.objectToZoom.addEventListener("mousemove", moveLens);
+            _this.lensWindow.addEventListener("touchmove", moveLens);
+            _this.objectToZoom.addEventListener("touchmove", moveLens);
+        }
+    }
+
+    createLensWindow() {
+        this.lensWindow = document.createElement("div");
+        this.lensWindow.setAttribute("class", this.zoomLensSelector);
+        this.objectToZoom.parentElement.insertBefore(this.lensWindow, this.objectToZoom);
+        this.createdZoomLens = true;
+    }
+
+    createOutputWindow() {
+        this.coordinateX = this.resultOfZoom.offsetWidth / this.lensWindow.offsetWidth;
+        this.coordinateY = this.resultOfZoom.offsetHeight / this.lensWindow.offsetHeight;
+        this.resultOfZoom.style.backgroundImage = "url('" + this.dataURL + "')";
+        this.resultOfZoom.style.backgroundSize = (this.objectToZoom.width * this.coordinateX) + "px " + (this.objectToZoom.height * this.coordinateY) + "px";
+    }
+
+    showOutputWindow() {
+        const canvas = this.objectToZoom;
+        this.dataURL = canvas.toDataURL('image/png');
+        this.resultOfZoom.style.backgroundImage = "url('" + this.dataURL + "')";
+        this.resultOfZoom.style.display = DISPLAY_BLOCK;
+    }
+
+    showZoomLens() {
+        this.zoomLens = document.querySelector(`.${this.zoomLensSelector}`);
+        this.zoomLens.style.display = DISPLAY_BLOCK;
+    }
+
+    createImageSliderHandlers() {
+        const imageZoomContainer = document.querySelector(this.imageZoomContainer);
+        const nextImageButton = document.querySelector(NEXT_IMAGE_BUTTON);
+        const previousImageButton = document.querySelector(PREVIOUS_IMAGE_BUTTON);
+        imageZoomContainer.addEventListener('mouseleave', function () {
+            this.hideZoomElements();
+        }.bind(this), false);
+        nextImageButton.addEventListener('mouseover', function () {
+            this.hideZoomElements();
+        }.bind(this), false);
+        previousImageButton.addEventListener('mouseover', function () {
+            this.hideZoomElements();
+        }.bind(this), false);
+    }
+
+    hideZoomElements() {
+        if (this.resultOfZoom.style.display === DISPLAY_BLOCK) {
+            this.zoomLens.style.display = DISPLAY_NONE;
+            this.resultOfZoom.style.display = DISPLAY_NONE;
+        }
+    }
+}
+
+
